898a4a238c0af761a1ccf86f33ea97acf33c2a48
cwkhang <cwkhang@yahoo.com>
2023-11-29 23:24:28 +0800

    Update setupFunctions.sh

--- 01.scripts/installation/setupFunctions.sh
+++ 01.scripts/installation/setupFunctions.sh
@@ -779,7 +779,7 @@ generateProductsImageFromTemplate() {
 checkSetupTemplateBasicPrerequisites() {
 
   errCount=0
-  chmod u+x "${lSumBootstrap}"
+  
 
   if [ -z "${SUIF_INSTALL_INSTALLER_BIN+x}" ]; then
     logE "[setupFunctions.sh:checkSetupTemplateBasicPrerequisites()] - Variable SUIF_INSTALL_INSTALLER_BIN was not set!"
@@ -815,7 +815,7 @@ checkSetupTemplateBasicPrerequisites() {
       errCount=$((errCount+1))
       #return 21
     fi
-
+    chmod u+x "${SUIF_PATCH_SUM_BOOTSTRAP_BIN}"
     if [ ! -x "${SUIF_PATCH_SUM_BOOTSTRAP_BIN}" ]; then
       logE "[setupFunctions.sh:checkSetupTemplateBasicPrerequisites()] - Declared variable SUIF_PATCH_SUM_BOOTSTRAP_BIN=${SUIF_PATCH_SUM_BOOTSTRAP_BIN} does not point to a valid executable file."
       errCount=$((errCount+1))
